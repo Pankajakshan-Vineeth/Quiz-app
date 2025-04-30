f068ec4eae861dbdb3a09899a4bc424ce559ffec
Pankajakshan Vineeth <yamunapankaj99@gmail.com>
2025-04-30 11:06:26 +0530

    check answer functionality

--- src/Components/Quiz/Quiz.jsx
+++ src/Components/Quiz/Quiz.jsx
@@ -1,18 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Quiz.css";
+import { data } from '../../assets/data';
 
 const Quiz = () => {
+
+let [qstnNo, setQstnNo] = useState(0);
+let [question, setQuestion] = useState(data[qstnNo]);
+
+const checkingAnser= (e, ans)=>{
+
+  if (question.ans===ans) {
+    e.target.classList.add('correct')
+  }else{
+    e.target.classList.add('wrong')
+  }
+}
+
   return (
     <div className="container">
       <h1>Quiz App</h1>
       <hr />
       <div className="index">1 of 5 questions</div>
-      <h2>1. Which device is required for the Internet connection?</h2>
+      <h2>{qstnNo+1}. {question.question}</h2>
       <ul>
-        <li>Modem</li>
-        <li>Router</li>
-        <li>LAN Cable</li>
-        <li>Pen Drive</li>
+        <li onClick={(e)=>{checkingAnser(e,1)}}>{question.option1}</li>
+        <li onClick={(e)=>{checkingAnser(e,2)}}>{question.option2}</li>
+        <li onClick={(e)=>{checkingAnser(e,3)}}>{question.option3}</li>
+        <li onClick={(e)=>{checkingAnser(e,4)}}>{question.option4}</li>
       </ul>
       <button>Next</button>
     </div>
